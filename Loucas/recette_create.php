f1765558fee4d722b8187dfa7c830b108eba6ce0
Heiseneit <76598260+Heiseneit@users.noreply.github.com>
2023-05-31 03:02:22 +0200

    CRUD 3

--- Loucas/recette_create.php
+++ Loucas/recette_create.php
@@ -1,4 +1,56 @@
+<?php
+// On inclut la classe User pour pouvoir l'utiliser
+include("./class/User.php");
+
+// On démarre une session
+session_start();
+
+// On initialise la variable d'erreur à false
+$error = false;
+
+// Si l'utilisateur a cliqué sur le bouton de déconnexion, on détruit la session
+if (isset($_POST['Deco'])) {
+    session_unset();
+    session_destroy();
+}
+
+// Si l'utilisateur a soumis le formulaire de connexion, on essaie de le connecter
+if (isset($_POST['CONNECT'])) {
+
+    // On définit les informations de connexion à la base de données
+    $db_username = 'tropicana';
+    $db_password = 'Loucas025!';
+    $db_name = 'tropicana_bdd';
+    $db_host = 'mysql-tropicana.alwaysdata.net';
+
+    try {
+        // On se connecte à la base de données
+        $GLOBALS['bdd'] = new PDO("mysql:host=" . $db_host . ";dbname=" . $db_name . ";", $db_username, $db_password);
+
+        // On récupère les informations du formulaire et on les sécurise
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // On crée un nouvel objet User et on essaie de connecter l'utilisateur
+        $TheUser = new User(null, null, null);
+        $reponse = $TheUser->connexion($username, $password);
+
+        // Si la connexion a réussi, on stocke l'ID de l'utilisateur dans la session et on met l'erreur à false
+        if ($reponse) {
+            $_SESSION["id"] = $reponse;
+            $error = false;
+        } else {
+            // Sinon, on met un message d'erreur dans la variable $error
+            $error = "mauvais mdp ou login";
+        }
+    } catch (Exception $th) {
+        // En cas d'erreur lors de la connexion à la base de données, on affiche le message d'erreur
+        echo $th->getMessage();
+    }
+}
+?>
 <html>
+
 <head>
     <meta charset="utf-8">
     <!-- On importe le fichier de style CSS -->
@@ -10,7 +62,7 @@
         <?php
         // Si l'utilisateur est connecté, on affiche la page principale
         if (isset($_SESSION["id"])) {
-            echo "page principale";
+            echo "Connecté";
         ?>
             <!-- On affiche le bouton de déconnexion -->
             <form action="" method="POST">
@@ -41,6 +93,20 @@
 
         }
         ?>
+            <form action="" method="POST">
+                <div align="center">
+                    <!-- On affiche le logo de Tropicana -->
+                    <img src="images/AddRecette.png" alt="Ajouter une recette">
+                    <h1></h1>
+                    <label><b>Nom d'utilisateur</b></label>
+                    <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+                    <label><b>Mot de passe</b></label>
+                    <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+                    <input type="submit" id='submit' name="CONNECT" value='Ajouter la recette !'>
+
+            </form>
     </div>
 </body>
 
